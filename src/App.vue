b063da9e76d8aaf9dda42cd70f2f624e90f6883f
nannany <ymym1990ymym@gmail.com>
2019-11-04 01:03:46 +0900

    gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ReflectProps :year="year" :month="month" :time="time" />
+    <div>time{{ time }}</div>
+    <button @click="receiveEvent">SSE</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable no-console,vue/no-shared-component-data */
 
+import ReflectProps from "./components/ReflectProps";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    ReflectProps
+  },
+  data: {
+    year: undefined,
+    month: undefined,
+    time: undefined
+  },
+  methods: {
+    receiveEvent: function() {
+      const evtSource = new EventSource(
+        "https://webflux-sse-demo.herokuapp.com/stream-sse"
+      );
+      const that = this;
+      evtSource.addEventListener(
+        "periodic-event",
+        function(e) {
+          const data = JSON.parse(e.data);
+          that.year = data.year;
+          that.month = data.month;
+          that.time = data.time;
+          console.log(that.time);
+        },
+        false
+      );
+    }
+  },
+  mounted() {
+    this.receiveEvent();
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/ReflectProps.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>year: {{ year }}</div>
+    <div>month: {{ month }}</div>
+    <div>time: {{ time }}</div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-console */
+
+export default {
+  name: "ReflectProps",
+
+  props: {
+    year: Number,
+    month: Number,
+    time: String
+  },
+  watch: {
+    time: function() {
+      console.log(this.props.time);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
