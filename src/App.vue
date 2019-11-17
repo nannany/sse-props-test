62e1fc02b6626ec09e6bfe9755e558a6dd2c0473
nannany <ymym1990ymym@gmail.com>
2019-11-17 20:03:14 +0900

    動くところまでできた

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,24 @@
   <div>
     <div>time:{{ time }}</div>
     <button @click="receiveEvent">SSE</button>
+    <div>{{ x }}</div>
+    <Map :x="this.x" :y="this.y" :guard_mode="this.guard_mode" />
   </div>
 </template>
 
 <script>
 /* eslint-disable no-console,vue/no-shared-component-data */
 
+import Map from "./components/Map";
 export default {
   name: "app",
+  components: { Map },
   data: function() {
     return {
-      time: undefined
+      time: undefined,
+      x: Number,
+      y: Number,
+      guard_mode: Number
     };
   },
   methods: {
@@ -28,7 +35,25 @@ export default {
         },
         false
       );
+    },
+    receivePlaceEvent: function() {
+      const evtSource = new EventSource(
+        "https://webflux-sse-demo.herokuapp.com/notification/events"
+      );
+      evtSource.addEventListener(
+        "receive/roboticbase-event",
+        e => {
+          const data = JSON.parse(e.data);
+          this.x = data.x;
+          this.y = data.y;
+          this.guard_mode = data.guard_mode;
+        },
+        false
+      );
     }
+  },
+  created() {
+    this.receivePlaceEvent();
   }
 };
 </script>
---
+++ src/components/Map.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="map_area">
+    <div>{{ x }}</div>
+    <div>{{ y }}</div>
+    <div>{{ guard_mode }}</div>
+    <img :src="blobUrl" />
+    <img class="place" :style="styleObject" src="/place.svg" />
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-console */
+
+import axios from "axios";
+
+export default {
+  name: "Map",
+  data: function() {
+    return {
+      blobUrl: String,
+      styleObject: {
+        top: "500px",
+        left: "500px"
+      }
+    };
+  },
+  props: {
+    x: undefined,
+    y: undefined,
+    guard_mode: undefined
+  },
+  methods: {
+    getMap: function() {
+      axios({
+        method: "GET",
+        url: "https://webflux-sse-demo.herokuapp.com/map",
+        responseType: "blob"
+      }).then(response => {
+        this.blobUrl = URL.createObjectURL(response.data);
+      });
+    },
+    updatePlace: () => {
+      console.log(this.styleObject);
+      this.styleObject.push({ top: this.x + "px", left: this.y + "px" });
+    }
+  },
+  computed: {
+    calculateXY: function() {
+      console.log("computed" + this.x);
+      return this.x + ":" + this.y;
+    }
+  },
+  created() {
+    this.getMap();
+  },
+  mounted() {
+    console.log("mounted" + this.x);
+    this.updatePlace();
+  },
+  watch: {
+    calculateXY: function() {
+      console.log("thisx" + this.x + ":thisy" + this.y);
+      this.styleObject = { left: this.x + "px", top: this.y + "px" };
+      this.blobUrl = this.x;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.map_area {
+  position: relative;
+  border: solid black;
+  height: 500px;
+  width: 500px;
+}
+.map_area > .place {
+  position: absolute;
+}
+</style>
